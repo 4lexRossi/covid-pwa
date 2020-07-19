ea7491c4171a2bf78c5879b7e24818fff013cded
4lexRossi <devalexrossi@gmail.com>
2020-07-18 22:41:48 -0300

    add components and skell on index

---
+++ covid/src/components/index.js
@@ -0,0 +1,19 @@
+import {Card , CardContent } from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Typography from './Typography'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import Menuitem from './Menuitem'
+
+
+export {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
